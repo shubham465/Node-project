4b3a53f3f63b8ddba9fcb2742173ba452ecb3ff1
shubham465 <55125061+shubham465@users.noreply.github.com>
2025-06-09 10:22:45 +0000

    feat: initialize Node.js project with Express, add essential dependencies and setup server

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 connectDb()
-app.use(cors()); 
+app.use(cors());
 app.use(express.json());
 app.use('/api/todos', require('./Routes/todoRoutes'))
 app.use('/api/user', require('./Routes/userRoutes'))

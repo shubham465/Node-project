db053d99252c1eeedadf9754bc503efb207faf61
shubham465 <55125061+shubham465@users.noreply.github.com>
2025-06-09 11:28:39 +0000

    fix: update .gitignore and correct server.js path in Vercel configuration

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.use('/api/user', require('./Routes/userRoutes'))
 app.get("/", (req, res) => res.send("Express on Vercel"));
 app.use(ErrorHandler)
 
-app.listen(port, ()=> {
-    console.log(`App is running on port : ${port}`)
-})
+module.exports = app;
+
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`App is running on port : ${port}`);
+  });
+}
